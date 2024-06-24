ef96238f08d54d89862a281836daa80ec6bac5fd
Nicolas <72094122+magicniclus@users.noreply.github.com>
2024-06-24 19:20:30 +0200

    Create page.tsx

---
+++ src/app/politique/page.tsx
@@ -0,0 +1,19 @@
+import Footer from "@/components/perso/footer/Footer";
+import Header from "@/components/perso/header/Header";
+
+const page = () => {
+  return (
+    <>
+      <Header />
+      <main className="min-h-screen flex justify-center items-center flex-col bg-slate-50">
+        -{" "}
+        <h1 className="text-3xl font-bold text-center">
+          - Politique de confidentialité -{" "}
+        </h1>
+      </main>
+      <Footer />
+    </>
+  ); // 1
+};
+
+export default page;
