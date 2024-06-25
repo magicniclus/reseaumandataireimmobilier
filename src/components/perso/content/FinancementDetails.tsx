f959cad5c1b265ebcc87b62433a889d5dab7571d
Nicolas <72094122+magicniclus@users.noreply.github.com>
2024-06-25 15:03:06 +0200

    Create FinancementDetails.tsx

---
+++ src/components/perso/content/FinancementDetails.tsx
@@ -0,0 +1,42 @@
+const FinancementDetails = () => {
+  return (
+    <section className="w-full my-16 md:my-24">
+      <div className="w-full max-w-5xl mx-auto px-4 md:px-6">
+        <p className="text-slate-500 text-xs">
+          *Un emprunt implique une obligation de remboursement. Assurez-vous de
+          pouvoir honorer vos engagements financiers avant de contracter un
+          crédit.
+        </p>
+        <p className="text-slate-500 text-xs mt-3">
+          Offre de financement pour des achats allant de 1 500 € à 75 000 €,
+          avec des périodes de remboursement de 12 à 180 mois à un Taux Annuel
+          Effectif Global (TAEG) fixe de 0%. Modalités applicables à partir du
+          01/01/2024. Par exemple, pour un achat avec un crédit de 10 000 €, le
+          remboursement se fait via 120 mensualités de 83,34 € à un TAEG fixe de
+          0% (taux nominal fixe : 0%). Le montant total dû par l&apos;emprunteur
+          est de 10 000 €, sans intérêts ni frais de dossier.
+        </p>
+        <p className="text-slate-500 text-xs mt-3">
+          L’option d’assurance facultative couvrant Décès, Perte Totale et
+          Irréversible d’Autonomie, ainsi que Maladie-Accident, proposée par
+          Cardif Assurance Vie et Cardif Assurances Risques Divers, a un coût
+          mensuel additionnel de 7,76 €. Le coût total de cette assurance est de
+          931,20 €, avec un taux annuel effectif de l’assurance de 1,81%. Cette
+          simulation est indicative et non contractuelle. Conditions valides au
+          01/01/2024. Plus de détails sur les conditions d&apos;éligibilité pour
+          l’ECO-PTZ sont disponibles ici : Service Public - ECO-PTZ
+        </p>
+        <p className="text-slate-500 text-xs mt-3">
+          Cette offre est conditionnée à l&apos;analyse et à l&apos;acceptation
+          du dossier par Domofinance, société anonyme avec un capital de 53 000
+          010 €, inscrite au RCS de Paris sous le numéro 450 275 490, domiciliée
+          au 1 Bd Haussmann, 75009 Paris. Numéro ORIAS 07 026 850
+          (www.orias.fr). Vous disposez d&apos;un droit de rétractation détaillé
+          ici : Domofinance - Rétractation
+        </p>
+      </div>
+    </section>
+  );
+};
+
+export default FinancementDetails;
