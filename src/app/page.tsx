4af13ef6b9e387eb5d6bf6dc55092dc823cc3506
Nicolas <72094122+magicniclus@users.noreply.github.com>
2024-06-25 13:10:56 +0200

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,6 @@
+import PresentationImage from "@/components/perso/carousel/PresentationImage";
 import Avis from "@/components/perso/content/Avis";
+import Engagements from "@/components/perso/content/Engagements";
 import Footer from "@/components/perso/footer/Footer";
 import Hero from "@/components/perso/hero/Hero";
 import Header from "../components/perso/header/Header";
@@ -9,6 +11,8 @@ export default function Home() {
       <Header />
       <main className="w-full min-h-screen">
         <Hero />
+        <PresentationImage />
+        <Engagements />
         <Avis />
       </main>
       <Footer />
