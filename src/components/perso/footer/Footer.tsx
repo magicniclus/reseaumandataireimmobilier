b4a24b5e1a502f3593f01a888773963071299d25
Nicolas <72094122+magicniclus@users.noreply.github.com>
2024-06-25 14:24:13 +0200

    Update Footer.tsx

--- src/components/perso/footer/Footer.tsx
+++ src/components/perso/footer/Footer.tsx
@@ -1,8 +1,10 @@
 const Footer = () => {
   return (
-    <footer className="w-full mb-14 md:mb-0">
+    <footer className="w-full mb-20 md:mb-0 bg-slate-50 md:bg-white">
       <div className="w-full max-w-5xl mx-auto px-4 py-10 md:px-6 text-slate-700 flex justify-between flex-col md:flex-row">
-        <p className="text-sm">© 2021 Nom de mon site tous droits réservés.</p>
+        <p className="text-sm hidden md:block">
+          © 2021 Nom de mon site tous droits réservés.
+        </p>
         <div className="flex flex-col md:flex-row  mt-5 md:mt-0">
           <a href="/mentions-legales" target="_blank" className="underline">
             Mentions légales
@@ -15,6 +17,9 @@ const Footer = () => {
             Politique de confidentialité
           </a>
         </div>
+        <p className="text-sm block md:hidden mt-5">
+          © 2021 Nom de mon site tous droits réservés.
+        </p>
       </div>
     </footer>
   );
