b42e32dca2b9bb095626d241d908a3d889083a04
Nicolas <72094122+magicniclus@users.noreply.github.com>
2024-06-25 14:16:02 +0200

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,37 @@
+"use client";
+
 import PresentationImage from "@/components/perso/carousel/PresentationImage";
 import Avis from "@/components/perso/content/Avis";
+import Beneficies from "@/components/perso/content/Beneficies";
 import Engagements from "@/components/perso/content/Engagements";
+import Financement from "@/components/perso/content/Financement";
 import Footer from "@/components/perso/footer/Footer";
 import Hero from "@/components/perso/hero/Hero";
+import { useEffect } from "react";
 import Header from "../components/perso/header/Header";
 
 export default function Home() {
+  useEffect(() => {
+    const handleSmoothScroll = (event: MouseEvent) => {
+      const target = event.target as HTMLElement;
+      if (target.matches("a[href^='#']")) {
+        event.preventDefault();
+        const id = target.getAttribute("href")?.slice(1);
+        if (id) {
+          const element = document.getElementById(id);
+          if (element) {
+            element.scrollIntoView({ behavior: "smooth" });
+          }
+        }
+      }
+    };
+
+    document.addEventListener("click", handleSmoothScroll);
+
+    return () => {
+      document.removeEventListener("click", handleSmoothScroll);
+    };
+  }, []);
   return (
     <>
       <Header />
@@ -13,6 +39,8 @@ export default function Home() {
         <Hero />
         <PresentationImage />
         <Engagements />
+        <Financement />
+        <Beneficies />
         <Avis />
       </main>
       <Footer />
