cd924ac33626c293ac5bfaf08b5fac07c162b0ac
Nicolas <72094122+magicniclus@users.noreply.github.com>
2024-06-25 15:03:00 +0200

    Update Financement.tsx

--- src/components/perso/content/Financement.tsx
+++ src/components/perso/content/Financement.tsx
@@ -1,7 +1,7 @@
 /* eslint-disable @next/next/no-img-element */
 const Financement = () => {
   return (
-    <section className="w-full py-24">
+    <section className="w-full py-24 md:py-16">
       <div className="max-w-5xl w-full px-4 md:px-6 mx-auto flex flex-col md:flex-row justify-between items-center h-min">
         <div className="w-full md:w-1/2 md:pr-6 flex flex-col justify-between">
           <h2 className="text-2xl text-slate-700 font-bold">
