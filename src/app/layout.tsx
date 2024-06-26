30e92757714be51137ae409f16512629e0156605
Nicolas <72094122+magicniclus@users.noreply.github.com>
2024-06-26 08:47:27 +0200

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -15,7 +15,10 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="fr">
+      <head>
+        <link rel="icon" href="/favicon.png" sizes="any" />
+      </head>
       <body className={inter.className}>{children}</body>
     </html>
   );
