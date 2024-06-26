67b30d622c4ac2213090366f47270ef3a592de5a
Nicolas <72094122+magicniclus@users.noreply.github.com>
2024-06-26 12:17:16 +0200

    Create layout.tsx

---
+++ src/app/mentions-legales/layout.tsx
@@ -0,0 +1,23 @@
+import "@/app/globals.css";
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+  title: "Eco Nouvelle Aquitaine - Mentions légales",
+  description:
+    "Isolaez votre maison avec TeraBois et économisez sur vos factures d'énergie.",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="fr">
+      <body className={inter.className}>{children}</body>
+    </html>
+  );
+}
