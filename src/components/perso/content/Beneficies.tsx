4373cc9d7fecc8f25739cf904bf788b433a1a8b5
Nicolas <72094122+magicniclus@users.noreply.github.com>
2024-06-26 12:49:06 +0200

    update img

--- src/components/perso/content/Beneficies.tsx
+++ src/components/perso/content/Beneficies.tsx
@@ -8,53 +8,34 @@ const Beneficies = () => {
         <div className=" flex flex-col md:flex-row justify-between items-center mt-12">
           <div className="w-full md:w-6/12 md:pr-6">
             <img
-              src="https://via.placeholder.com/500"
+              src="/global.jpg"
               className="w-full h-auto max-h-[300px] rounded-lg object-cover"
               alt="Financement"
             />
           </div>
           <div className="w-full md:w-6/12 md:pl-6 flex flex-col justify-between mt-12 md:mt-0">
             <h2 className="text-2xl text-slate-700 font-bold">
-              Obtenez un financement pour vos rénovations à{" "}
-              <span className="text-yellow">0% d&apos;intérêt**</span>
+              Les bénéfices de la{" "}
+              <span className="text-yellow">
+                rénovation énergétique avec ECO-Nouvelle-Aquitaine
+              </span>
             </h2>
             <ul className="list-inside text-xl mt-7">
               <li className="flex">
-                <CheckIcon className="h-7 w-7 min-w-[28px] mr-2 mb-2" />
-                <p>
-                  <span className="font-bold text-yellow-500">
-                    Je soumets ma demande
-                  </span>{" "}
-                  et je serai contacté par un conseiller pour discuter de mon
-                  projet.
-                </p>
+                <CheckIcon className="h-7 w-7 min-w-[28px] mr-2 mb-2 text-yellow-500" />
+                <p>Un confort optimum en toutes saisons</p>
               </li>
               <li className="mt-5 flex">
-                <CheckIcon className="h-7 w-7 min-w-[28px] mr-2  mb-2" />
-                <p>
-                  <span className="font-bold text-yellow-500">
-                    Mon conseiller prépare une étude
-                  </span>{" "}
-                  adaptée aux spécificités de mon logement.
-                </p>
+                <CheckIcon className="h-7 w-7 min-w-[28px] mr-2  mb-2 text-yellow-500" />
+                <p>Importantes économies sur vos factures énergétiques</p>
               </li>
               <li className="mt-5 flex">
-                <CheckIcon className="h-7 w-7 min-w-[28px] mr-2  mb-2" />
-                <p>
-                  <span className="font-bold text-yellow-500">
-                    Les aides financières sont directement appliquées
-                  </span>{" "}
-                  sur le devis.
-                </p>
+                <CheckIcon className="h-7 w-7 min-w-[28px] mr-2  mb-2 text-yellow-500" />
+                <p>Meilleure classement énergétique sur votre DPE</p>
               </li>
               <li className="mt-5 flex">
-                <CheckIcon className="h-7 w-7 min-w-[28px] mr-2  mb-2" />
-                <p>
-                  <span className="font-bold text-yellow-500">
-                    Un technicien d&apos;ECO-Nouvelle-Aquitaine
-                  </span>{" "}
-                  supervise la réalisation des travaux.
-                </p>
+                <CheckIcon className="h-7 w-7 min-w-[28px] mr-2  mb-2 text-yellow-500" />
+                <p>Valorisation de votre logement</p>
               </li>
             </ul>
             <a
