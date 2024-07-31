f581b038c74b11a11fc9fa25f1ccefeb07559456
Nicolas <72094122+magicniclus@users.noreply.github.com>
2024-07-31 10:09:42 +0200

    Update tailwind.config.ts

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -11,6 +11,16 @@ const config = {
     "./app/**/*.{ts,tsx}",
     "./src/**/*.{ts,tsx}",
   ],
+  keyframes: {
+    shimmer: {
+      from: {
+        backgroundPosition: "0 0",
+      },
+      to: {
+        backgroundPosition: "-200% 0",
+      },
+    },
+  },
   prefix: "",
   theme: {
     container: {
