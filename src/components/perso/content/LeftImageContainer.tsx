0790cc414f949e6b4e99c430ad697fd60616abc5
Nicolas <72094122+magicniclus@users.noreply.github.com>
2024-08-01 19:39:57 +0200

    Create LeftImageContainer.tsx

---
+++ src/components/perso/content/LeftImageContainer.tsx
@@ -0,0 +1,169 @@
+/* eslint-disable @next/next/no-img-element */
+const LeftImageContainer = () => {
+  return (
+    <div className="bg-white">
+      <div className="mx-auto max-w-7xl px-4 py-24 md:py-16 sm:px-6 lg:px-8">
+        <div className="w-full justify-between items-center flex flex-col md:flex-row">
+          <div className="w-full md:w-1/2">
+            <h3 className="text-2xl text-yellow-500 font-bold">
+              Exercer un métier qui a du sens
+            </h3>
+            <p className="mt-8">
+              Être <span className="font-bold">conseiller immobilier</span>,
+              c’est jouer un rôle central dans les projets de vie des gens :
+              guider, orienter et conseiller. Ce rôle
+              <span className="font-bold">indispensable</span> apporte une
+              dimension significative à votre activité quotidienne !
+            </p>
+            <p className="mt-5">
+              En tant que{" "}
+              <span className="font-bold">conseiller immobilier</span> avec{" "}
+              <span className="font-bold">eXp</span>, vous devenez votre propre
+              patron ! Vous gérez votre emploi du temps et développez votre
+              entreprise à{" "}
+              <span className="font-bold">votre propre rythme</span>. Vous avez
+              la liberté de vous organiser pour équilibrer vos engagements
+              professionnels et vos activités personnelles.
+            </p>
+          </div>
+          <div className="w-full md:w-1/2 flex items-center justify-center mt-10 md:mt-0">
+            <img
+              src="/3.png"
+              alt="sens"
+              className="w-[70%] h-auto max-h-[250px] object-contain"
+            />
+          </div>
+        </div>
+        <div className="w-full justify-between items-center flex flex-col md:flex-row mt-16">
+          <div className="w-full md:w-1/2 hidden md:flex items-center justify-center">
+            <img
+              src="/1.png"
+              alt="sens"
+              className="w-[70%] h-auto max-h-[250px] object-contain"
+            />
+          </div>
+          <div className="w-full md:w-1/2 mt-10 md:mt-0">
+            <h3 className="text-2xl text-yellow-500 font-bold">
+              Bénéficiez d&apos;un Potentiel de Commission Attractif
+            </h3>
+            <p className="mt-8">
+              Avec <span className="font-bold">eXp</span>, votre rémunération
+              est à la hauteur de votre investissement et peut{" "}
+              <span className="font-bold">atteindre des sommets</span> !
+            </p>
+            <p className="mt-5">
+              Vous profitez de 3 sources de revenus attractives :
+            </p>
+            <ul className="mt-5 disc">
+              <li className="mt-3">
+                Une commission basée sur les performances de votre organisation
+                commerciale
+              </li>
+              <li className="mt-3">
+                La possibilité de travailler avec votre propre agence
+                immobilière et de maximiser votre indépendance et vos revenus
+              </li>
+            </ul>
+            <p className="mt-5">
+              Rejoignez eXp et découvrez un modèle de rémunération conçu pour
+              récompenser votre travail acharné et votre dévouement !
+            </p>
+          </div>
+          <div className="w-full md:w-1/2 flex md:hidden items-center justify-center  mt-10 md:mt-0">
+            <img
+              src="/1.png"
+              alt="sens"
+              className="w-[70%] h-auto max-h-[250px] object-contain"
+            />
+          </div>
+        </div>
+        <div className="w-full justify-between items-center flex flex-col md:flex-row mt-16">
+          <div className="w-full md:w-1/2">
+            <h3 className="text-2xl text-yellow-500 font-bold">
+              Apprendre et Être Accompagné
+            </h3>
+            <p className="mt-8">
+              Que vous soyez novice ou expérimenté dans l&apos;immobilier, chez
+              <span className="font-bold">eXp</span>, vous bénéficiez d&apos;un
+              accompagnement et d&apos;une formation continus. Des bases du
+              métier aux <span className="font-bold">compétences avancées</span>
+              , vous apprenez et vous perfectionnez tout au long de
+              <span className="font-bold">votre carrière</span> !
+            </p>
+            <p className="mt-5">
+              Vous profitez d&apos;un accompagnement constant de la part de
+              votre mentor sur le terrain, d&apos;un programme de{" "}
+              <span className="font-bold">formation complet</span> adapté à vos
+              besoins, ainsi que de formations en ligne ou en présentiel, à
+              votre propre rythme !
+            </p>
+          </div>
+          <div className="w-full md:w-1/2 flex items-center justify-center mt-10 md:mt-0">
+            <img
+              src="/4.png"
+              alt="sens"
+              className="w-[70%] h-auto max-h-[250px] object-contain"
+            />
+          </div>
+        </div>
+        <div className="w-full justify-between items-center flex flex-col md:flex-row mt-16">
+          <div className="w-full md:w-1/2 hidden md:flex items-center justify-center">
+            <img
+              src="/2.png"
+              alt="sens"
+              className="w-[70%] h-auto max-h-[250px] object-contain"
+            />
+          </div>
+          <div className="w-full md:w-1/2">
+            <h3 className="text-2xl text-yellow-500 font-bold">
+              Bénéficier des Clés pour Votre Réussite
+            </h3>
+            <p className="mt-8">
+              eXp met à disposition de ses conseillers immobiliers des outils,
+              services et partenariats pour assurer leur succès.
+            </p>
+            <p className="mt-5">
+              Vous profitez de 3 sources de revenus attractives :
+            </p>
+            <ul className="mt-5 disc">
+              <li>
+                <span className="font-bold"> Soutien Administratif : </span>
+                Un gestionnaire administratif sera à vos côtés pour vous
+                assister dans vos tâches administratives quotidiennes.
+              </li>
+              <li className="mt-3">
+                <span className="font-bold">
+                  {" "}
+                  Outils Digitaux Performants :
+                </span>{" "}
+                Profitez d&apos;outils numériques exclusifs et efficaces pour
+                gagner du temps et vous concentrer sur votre activité
+                principale.
+              </li>
+              <li className="mt-3">
+                <span className="font-bold"> Réseau de Partenaires : </span>{" "}
+                Notre réseau de partenaires vous aidera à offrir un large
+                éventail de services à vos clients, augmentant ainsi votre
+                valeur ajoutée et votre réussite professionnelle.
+              </li>
+            </ul>
+          </div>
+          <div className="w-full md:w-1/2 flex md:hidden items-center justify-center mt-10 md:mt-0">
+            <img
+              src="/2.png"
+              alt="sens"
+              className="w-[70%] h-auto max-h-[250px] object-contain"
+            />
+          </div>
+        </div>
+        <div className="w-full mt-10 mx-auto flex justify-center">
+          <a className=" p-3 bg-blue-700 text-white rounded-md" href="#form">
+            Demander un rendez-vous
+          </a>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default LeftImageContainer;
