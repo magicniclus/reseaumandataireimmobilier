047be403faf5a2ab9dd8989404b66f9677654ff7
Nicolas <72094122+magicniclus@users.noreply.github.com>
2024-08-02 10:52:22 +0200

    Update Footer.tsx

--- src/components/perso/footer/Footer.tsx
+++ src/components/perso/footer/Footer.tsx
@@ -3,7 +3,7 @@ const Footer = () => {
     <footer className="w-full mb-20 md:mb-0 bg-slate-50 md:bg-white">
       <div className="w-full max-w-5xl mx-auto px-4 py-10 md:px-6 text-slate-700 flex justify-between flex-col md:flex-row">
         <p className="text-sm hidden md:block">
-          © 2021 Nom de mon site tous droits réservés.
+          © 2024 reseau-mandataire-immobilier.fr tous droits réservés.
         </p>
         <div className="flex flex-col md:flex-row  mt-5 md:mt-0">
           <a href="/mentions-legales" target="_blank" className="underline">
@@ -18,7 +18,7 @@ const Footer = () => {
           </a>
         </div>
         <p className="text-sm block md:hidden mt-5">
-          © 2021 Nom de mon site tous droits réservés.
+          © 2024 reseau-mandataire-immobilier.fr tous droits réservés.
         </p>
       </div>
     </footer>
