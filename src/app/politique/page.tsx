1aafba3b2f203e13727b0230b4df3916fee1e590
Nicolas <72094122+magicniclus@users.noreply.github.com>
2024-08-02 17:03:38 +0200

    Update page.tsx

--- src/app/politique/page.tsx
+++ src/app/politique/page.tsx
@@ -15,9 +15,9 @@ const page = () => {
             Introduction
           </h2>
           <p className="text-nomral font-normal mt-5">
-            Bienvenue sur TeraBois. Nous nous engageons à protéger votre vie
-            privée et à respecter les normes de protection des données
-            personnelles en vigueur.
+            Bienvenue sur Reseau-mandataire-immobilier. Nous nous engageons à
+            protéger votre vie privée et à respecter les normes de protection
+            des données personnelles en vigueur.
           </p>
           <p className="text-nomral font-normal mt-5">
             La présente politique de confidentialité a pour but de vous informer
@@ -31,41 +31,44 @@ const page = () => {
             Collecte des informations
           </h2>
           <p className="text-nomral font-normal mt-5">
-            TeraBois collecte des informations que vous nous fournissez lorsque
-            vous remplissez des formulaires sur notre site web, par exemple
-            lorsque vous demandez un devis pour une rénovation. Les informations
-            collectées peuvent inclure votre nom, adresse e-mail, numéro de
-            téléphone, adresse et autres détails relatifs à votre projet de
-            rénovation.
+            Reseau-mandataire-immobilier collecte des informations que vous nous
+            fournissez lorsque vous remplissez des formulaires sur notre site
+            web, par exemple lorsque vous demandez un devis pour une rénovation.
+            Les informations collectées peuvent inclure votre nom, adresse
+            e-mail, numéro de téléphone, adresse et autres détails relatifs à
+            votre projet de rénovation.
           </p>
           <h2 className="text-2xl font-bold tracking-tight text-gray-800 sm:text-3xl  mt-16">
             Utilisation des informations
           </h2>
           <p className="text-nomral font-normal mt-5">
-            TeraBois utilise les informations recueillies pour vous fournir des
-            devis précis, répondre à vos questions, communiquer avec vous au
-            sujet de nos services, et améliorer nos offres et services.
+            Reseau-mandataire-immobilier utilise les informations recueillies
+            pour vous fournir des devis précis, répondre à vos questions,
+            communiquer avec vous au sujet de nos services, et améliorer nos
+            offres et services.
           </p>
           <h2 className="text-2xl font-bold tracking-tight text-gray-800 sm:text-3xl  mt-16">
             Partage des informations
           </h2>
           <p className="text-nomral font-normal mt-5">
-            TeraBois est une entreprise qui récolte des leads pour son propre
-            compte et ne partage, ne vend, ni n&apos;échange vos informations
-            personnelles avec des tiers, sauf si la loi l&apos;exige, dans le
-            cadre d&apos;une transaction d&apos;entreprise, ou si nous avons
-            obtenu votre consentement préalable.
+            Reseau-mandataire-immobilier est une entreprise qui récolte des
+            leads pour son propre compte et ne partage, ne vend, ni
+            n&apos;échange vos informations personnelles avec des tiers, sauf si
+            la loi l&apos;exige, dans le cadre d&apos;une transaction
+            d&apos;entreprise, ou si nous avons obtenu votre consentement
+            préalable.
           </p>
           <h2 className="text-2xl font-bold tracking-tight text-gray-800 sm:text-3xl  mt-16">
             Sécurité des informations
           </h2>
           <p className="text-nomral font-normal mt-5">
-            TeraBois utilise des mesures de sécurité appropriées pour protéger
-            contre la perte, l&apos;abus et l&apos;altération des données
-            utilisées par notre système. Cependant, aucune transmission de
-            données sur Internet n&apos;est 100% sécurisée, et bien que nous
-            nous efforçons de protéger vos informations personnelles, nous ne
-            pouvons garantir leur sécurité absolue.
+            Reseau-mandataire-immobilier utilise des mesures de sécurité
+            appropriées pour protéger contre la perte, l&apos;abus et
+            l&apos;altération des données utilisées par notre système.
+            Cependant, aucune transmission de données sur Internet n&apos;est
+            100% sécurisée, et bien que nous nous efforçons de protéger vos
+            informations personnelles, nous ne pouvons garantir leur sécurité
+            absolue.
           </p>
           <h2 className="text-2xl font-bold tracking-tight text-gray-800 sm:text-3xl  mt-16">
             Vos droits
@@ -79,17 +82,17 @@ const page = () => {
           <p className="text-nomral font-normal mt-5">
             Pour exercer ces droits, ou pour toute question ou préoccupation
             concernant cette politique de confidentialité, veuillez nous
-            contacter à contact@terabois.fr.
+            contacter à contact@Reseau-mandataire-immobilier.fr.
           </p>
           <h2 className="text-2xl font-bold tracking-tight text-gray-800 sm:text-3xl  mt-16">
             Changements à cette politique de confidentialité
           </h2>
           <p className="text-nomral font-normal mt-5">
-            TeraBois se réserve le droit de modifier cette politique de
-            confidentialité à tout moment. Toutes les modifications seront
-            publiées sur cette page, avec la date de la dernière mise à jour.
-            Veuillez consulter régulièrement cette politique de confidentialité
-            pour être informé de toute modification.
+            Reseau-mandataire-immobilier se réserve le droit de modifier cette
+            politique de confidentialité à tout moment. Toutes les modifications
+            seront publiées sur cette page, avec la date de la dernière mise à
+            jour. Veuillez consulter régulièrement cette politique de
+            confidentialité pour être informé de toute modification.
           </p>
           <p className="text-nomral font-normal mt-5">
             Dernière mise à jour : 30/05/2023
@@ -106,7 +109,8 @@ const page = () => {
           </p>
           <p className="text-nomral font-normal mt-5">
             Pour toute question relative à notre politique de confidentialité,
-            n&apos;hésitez pas à nous contacter à contact@terabois.fr.
+            n&apos;hésitez pas à nous contacter à
+            contact@Reseau-mandataire-immobilier.fr.
           </p>
         </div>
       </main>
