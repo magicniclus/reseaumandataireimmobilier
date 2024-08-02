7c3f2116bf12bc30e0c262641e836c1d94598753
Nicolas <72094122+magicniclus@users.noreply.github.com>
2024-08-02 17:03:46 +0200

    Update FAQ.tsx

--- src/components/perso/content/FAQ.tsx
+++ src/components/perso/content/FAQ.tsx
@@ -12,6 +12,28 @@ const faqs = [
       "Chez eXp Realty, la mission principale d'un agent est de fournir un service exceptionnel à ses clients en matière d'achat, de vente et de location de biens immobiliers, tout en bénéficiant du soutien et des ressources d'un réseau global.",
   },
   {
+    question: "C'est quoi la team Reseau d'eXp?",
+    answer:
+      "La team Reseau d'eXp est un groupe d'agents immobiliers indépendants qui travaillent ensemble pour partager des ressources, des outils uniques, des connaissances et des opportunités d'affaires, tout en bénéficiant du soutien et de la formation de l'équipe eXp Realty.",
+  },
+  {
+    question: "C'est quoi Avenue-immo",
+    answer:
+      "Avenue-immo est un site d'estimaiton immobiliere en ligne créé par le fondateur de la team Réseau, qui permet aux membres de l'équipe de générer des leads qualifiés et de maximiser leurs opportunités d'affaires à prix coutant et donc de dominer leurs marché local.",
+  },
+  {
+    question:
+      "Quels sont les avantages de devenir un agent immobilier indépendant?",
+    answer:
+      "En devenant un agent immobilier indépendant, vous avez la liberté de gérer votre propre emploi du temps, de fixer vos propres objectifs et de maximiser vos revenus potentiels en fonction de vos efforts et de votre dévouement.",
+  },
+  {
+    question:
+      "Qu'est-ce qui distingue eXp Realty des autres agences immobilières?",
+    answer:
+      "eXp Realty se distingue par son modèle d'agence immobilière virtuelle, qui offre aux agents la possibilité de travailler de n'importe où, de bénéficier d'un programme de partage des revenus, et d'accéder à une plateforme technologique de pointe.",
+  },
+  {
     question: "Comment devenir un agent chez eXp Realty?",
     answer:
       "Pour devenir un agent chez eXp Realty, vous devez d'abord obtenir votre licence immobilière. Ensuite, vous pouvez rejoindre notre réseau en suivant notre processus d'intégration qui inclut une formation complète et un mentorat.",
