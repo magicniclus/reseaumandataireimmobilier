cd612ff232ae926b838581b2825fbbe5504ea5eb
Nicolas <72094122+magicniclus@users.noreply.github.com>
2024-09-04 16:02:40 +0200

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -7,7 +7,7 @@ const inter = Inter({ subsets: ["latin"] });
 export const metadata: Metadata = {
   title: "Reseau mandaitaire immobilier by eXp",
   description:
-    "Devenez mandataire immobilier au sein de la meilleur équipe du meilleur reseau",
+    "Devenez mandataire immobilier au sein de la meilleure équipe du meilleur réseau",
 };
 
 export default function RootLayout({
@@ -19,6 +19,13 @@ export default function RootLayout({
     <html lang="fr">
       <head>
         <link rel="icon" href="/favicon.png" sizes="any" />
+        <script
+          async
+          src="https://www.googletagmanager.com/gtag/js?id=G-5JB9HKG2JG"
+        ></script>
+        <script>
+          {`window.dataLayer = window.dataLayer || []; function gtag(){dataLayer.push(arguments);} gtag('js', new Date()); gtag('config', 'G-5JB9HKG2JG');`}
+        </script>
       </head>
       <body className={inter.className}>{children}</body>
     </html>
