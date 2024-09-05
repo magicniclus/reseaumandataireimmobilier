d6378518f14e40b3edb71c695788f3d57d0d740f
Nicolas <72094122+magicniclus@users.noreply.github.com>
2024-09-05 13:17:50 +0200

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -20,19 +20,6 @@ export default function RootLayout({
     <html lang="fr">
       <head>
         <link rel="icon" href="/favicon.png" sizes="any" />
-
-        {/* Google Tag Manager */}
-        <script>{`(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);})(window,document,'script','dataLayer','GTM-KF675HP7');`}</script>
-        {/* End Google Tag Manager */}
-
-        {/* Google tag (gtag.js) */}
-        <script
-          async
-          src="https://www.googletagmanager.com/gtag/js?id=G-5JB9HKG2JG"
-        ></script>
-        <script>
-          {`window.dataLayer = window.dataLayer || []; function gtag(){dataLayer.push(arguments);} gtag('js', new Date()); gtag('config', 'G-5JB9HKG2JG');`}
-        </script>
       </head>
       <body className={inter.className}>
         {/* Google Tag Manager (noscript) */}
